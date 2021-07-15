f063449e4e6ce6ba836318a170b6de009895da09
kiss8981 <kdh89811@gmail.com>
2021-07-15 12:16:09 +0900

    [ADD] 1주차 에러처리

---
+++ 1주차 미션 목적 - 파이썬 프로그래밍에 대한 동기부여/다른_Error_3가지.py
@@ -0,0 +1,9 @@
+# NameError 
+print(test)
+
+# IndexError
+a = [1, 2, 3]
+print(a[3])
+
+# ZeroDivisionError
+print(10/0)
---
+++ 1주차 미션 목적 - 파이썬 프로그래밍에 대한 동기부여/다른_Error_3가지_디버깅.py
@@ -0,0 +1,10 @@
+# NameError
+test = 10
+print(test)
+
+# IndexError
+a = [10, 20, 30]
+print(a[2])
+
+# ZeroDivisionError
+print(10/1)
