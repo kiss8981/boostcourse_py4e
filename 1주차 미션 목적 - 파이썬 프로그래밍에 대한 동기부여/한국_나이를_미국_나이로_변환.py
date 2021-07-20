4220865c2cd1055ae065f2323bd52a31cba601d6
kiss8981 <kdh89811@gmail.com>
2021-07-20 14:29:08 +0900

    [FIX] 한국나이 미국나이 수정

--- 1주차 미션 목적 - 파이썬 프로그래밍에 대한 동기부여/한국_나이를_미국_나이로_변환.py
+++ 1주차 미션 목적 - 파이썬 프로그래밍에 대한 동기부여/한국_나이를_미국_나이로_변환.py
@@ -1,6 +1,6 @@
 age = int(input("나이를 적어주세요 : "))
 birth = int(input("생일이 지났습니까? 맞으면 0 아니면 -1 : "))
 if birth == 0:
-    print(f"한국나이는 {age}살 입니다")
+    print(f"미국나이는 {age}살 입니다")
 if birth == -1:
-    print(f"한국나이는 {age - 1}살 입니다")
+    print(f"미국나이는 {age - 1}살 입니다")
